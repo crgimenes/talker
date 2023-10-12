d991ba5dd20ee03489f5303f5534885e45cc5d67
crgimenes <crg@crg.eti.br>
2023-10-11 23:59:31 -0300

    add handler skeleton

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"talker/config"
 	"talker/session"
+	"text/template"
 	"time"
 )
 
@@ -13,8 +16,84 @@ const cookieName = "session_talker"
 
 var (
 	GitTag string = "dev"
-	sc     *session.Control
+
+	//go:embed assets/*
+	assets embed.FS
 )
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	sid, sd, ok := session.SC.Get(r)
+	if !ok {
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
+	}
+
+	// renew session
+	session.SC.Save(w, sid, sd)
+
+	//////////////////////////
+
+	index, err := assets.ReadFile("assets/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	t, err := template.New("index.html").Parse(string(index))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// exec template
+	err = t.Execute(w, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// http.Redirect(w, r, "/payments", http.StatusFound)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		index, err := assets.ReadFile("assets/login.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		t, err := template.New("login.html").Parse(string(index))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// exec template
+		err = t.Execute(w, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		return
+	}
+
+	// login logic
+
+	// create session
+	sid, sd := session.SC.Create()
+
+	// save session
+	session.SC.Save(w, sid, sd)
+
+}
+
+func logoutHandler(w http.ResponseWriter, r *http.Request) {
+	sid, _, ok := session.SC.Get(r)
+	if !ok {
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
+	}
+
+	// remove session
+	session.SC.Delete(w, sid)
+
+	http.Redirect(w, r, "/login", http.StatusFound)
+}
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
@@ -34,4 +113,20 @@ func main() {
 		}
 	}()
 
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/login", loginHandler)
+	mux.HandleFunc("/logout", logoutHandler)
+
+	s := &http.Server{
+		Handler:        mux,
+		Addr:           config.CFG.Listen,
+		ReadTimeout:    5 * time.Second,
+		WriteTimeout:   5 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Printf("Listening on port %s\n", config.CFG.Listen)
+	log.Fatal(s.ListenAndServe())
 }
