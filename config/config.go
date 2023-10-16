0a766d9410dc0990922f3225bc2ce7a74149e343
crgimenes <crg@crg.eti.br>
2023-10-16 10:43:27 -0300

    add secure parameters

--- config/config.go
+++ config/config.go
@@ -6,9 +6,13 @@ import (
 )
 
 type Config struct {
-	Listen      string `ini:"listen" cfg:"listen" cfgDefault:"0.0.0.0:2200"`
-	DatabaseURL string `ini:"database_url" cfg:"database_url" cfgRequired:"true"`
-	FilesPath   string `ini:"files_path" cfg:"files_path" cfgRequired:"true"`
+	Salt          string `ini:"salt" cfg:"salt" cfgRequired:"true" cfgHelper:"Salt used to hash passwords."`
+	Listen        string `ini:"listen" cfg:"listen" cfgDefault:"0.0.0.0:2200"`
+	DatabaseURL   string `ini:"database_url" cfg:"database_url" cfgRequired:"true"`
+	FilesPath     string `ini:"files_path" cfg:"files_path" cfgRequired:"true"`
+	SecureCookie  bool   `ini:"secure_cookie" cfg:"secure_cookie" cfgDefault:"false" cfgHelper:"If true, the cookie will only be sent over HTTPS connections, never over plain HTTP."`
+	MaxAgeSession int    `ini:"max_age_session" cfg:"max_age_session" cfgDefault:"86400" cfgHelper:"Max age of session in seconds."`
+	MaxUploadSize int64  `ini:"max_upload_size" cfg:"max_upload_size" cfgDefault:"10485760" cfgHelper:"Max upload size in bytes."`
 }
 
 var CFG *Config
